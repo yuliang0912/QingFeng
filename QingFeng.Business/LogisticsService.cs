c84f5fd95d1bff2cd1ee13e328772b8ef740c59a
yuliang <4896819@qq.com>
2016-11-23 17:16:04 +0800

    新增物流信息

---
+++ QingFeng.Business/LogisticsService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QingFeng.Business
+{
+    public class LogisticsService
+    {
+        public List<KeyValuePair<int, string>> GetComplanyList()
+        {
+            return new List<KeyValuePair<int, string>>()
+            {
+                new KeyValuePair<int, string>(1, "顺丰速运"),
+                new KeyValuePair<int, string>(2, "邮政EMS"),
+                new KeyValuePair<int, string>(3, "圆通快递"),
+                new KeyValuePair<int, string>(4, "申通快递"),
+                new KeyValuePair<int, string>(5, "韵达快递"),
+                new KeyValuePair<int, string>(6, "中通快递"),
+                new KeyValuePair<int, string>(7, "宅急送"),
+                new KeyValuePair<int, string>(8, "天天快递"),
+            };
+        }
+    }
+}
---
+++ QingFeng.DataAccessLayer/Repository/LogisticsRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QingFeng.Models;
+
+namespace QingFeng.DataAccessLayer.Repository
+{
+    public class LogisticsRepository : RepositoryBase<LogisticsInfo>
+    {
+        const string TableName = "logistics";
+
+        public LogisticsRepository() : base(TableName)
+        {
+        }
+
+    }
+}
