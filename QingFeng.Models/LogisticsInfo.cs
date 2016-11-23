8e074e7b2ef45440ea8e3a04ae289f1123baa167
yuliang <4896819@qq.com>
2016-11-23 17:02:34 +0800

    新增物流信息

---
+++ QingFeng.Models/LogisticsInfo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using QingFeng.Common.Dapper;
+
+namespace QingFeng.Models
+{
+    public class LogisticsInfo
+    {
+        [IgnoreField]
+        public int Id { get; set; }
+
+        //订单号
+        public long OrderId { get; set; }
+
+        //物流公司ID
+        public int CompanyId { get; set; }
+
+        //物流公司
+        public string CompanyName { get; set; }
+
+        //物流单号
+        public string OddNumber { get; set; }
+
+        //运费
+        public decimal Price { get; set; }
+
+        public int Status { get; set; }
+
+        public DateTime CreateDate { get; set; }
+
+        public DateTime UpdateDate { get; set; }
+    }
+}
