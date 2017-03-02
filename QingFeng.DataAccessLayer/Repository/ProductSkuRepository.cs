4319ae038f63e73b8b47ff6eb7ddcf6e6e2c74a8
yuliang0912 <4896819@qq.com>
2017-03-02 23:56:18 +0800

    1

---
+++ QingFeng.DataAccessLayer/Repository/ProductSkuRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QingFeng.Models;
+
+namespace QingFeng.DataAccessLayer.Repository
+{
+    public class ProductSkuRepository : RepositoryBase<ProductSkus>
+    {
+        const string TableName = "productSkus";
+
+        public ProductSkuRepository() : base(TableName)
+        {
+
+            //add on duc.... update status ....
+        }
+    }
+}
