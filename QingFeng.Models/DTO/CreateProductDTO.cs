fb730561c5d1a573bbd7ec0fbf16272327d0423e
yuliang0912 <4896819@qq.com>
2017-03-02 23:57:54 +0800

    1

---
+++ QingFeng.Models/DTO/CreateProductDTO.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QingFeng.Common;
+
+namespace QingFeng.Models.DTO
+{
+    public class CreateProductDto
+    {
+        public string baseNo { get; set; }
+
+        public string baseName { get; set; }
+
+        public AgentEnums.Brand brandId { get; set; }
+
+        public AgentEnums.Category categoryId { get; set; }
+
+        public AgentEnums.Sex sex { get; set; }
+
+        public List<SubProduct> subProduct { get; set; }
+    }
+
+    public class SubProduct
+    {
+        public string color { get; set; }
+
+        public decimal lowestPrice { get; set; }
+
+        public List<SizeInfo> sizeList { get; set; }
+    }
+
+    public class SizeInfo
+    {
+        public int sizeId { get; set; }
+
+        public string sizeName { get; set; }
+
+        public decimal sizePrice { get; set; }
+    }
+}
