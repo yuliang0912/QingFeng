e125f29b5b2798f1a558356a52910384017adb1c
yuliang <4896819@qq.com>
2017-03-10 18:13:31 +0800

    1

---
+++ QingFeng.Models/DTO/ProductStockExcelDTO.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QingFeng.Models.DTO
+{
+    public class ProductStockExcelDTO
+    {
+        [JsonProperty(PropertyName = "商品ID")]
+        public int BaseId { get; set; }
+
+        [JsonProperty(PropertyName = "spu_id")]
+        public int ProductId { get; set; }
+
+        [JsonProperty(PropertyName = "sku_id")]
+        public int SkuId { get; set; }
+
+        [JsonProperty(PropertyName = "stock_id")]
+        public int StockId { get; set; }
+
+        [JsonProperty(PropertyName = "货号")]
+        public string BaseNo { get; set; }
+
+        [JsonProperty(PropertyName = "颜色")]
+        public string ProductNo { get; set; }
+
+        [JsonProperty(PropertyName = "尺码")]
+        public string SkuName { get; set; }
+
+        [JsonProperty(PropertyName = "库存")]
+        public int StockNum { get; set; }
+    }
+}
---
+++ QingFeng.Models/DTO/UserPriceExcelDTO.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+
+namespace QingFeng.Models.DTO
+{
+    public class UserPriceExcelDTO
+    {
+        [JsonProperty(PropertyName = "商品ID")]
+        public int BaseId { get; set; }
+
+        [JsonProperty(PropertyName = "spu_id")]
+        public int ProductId { get; set; }
+
+        [JsonProperty(PropertyName = "货号")]
+        public string BaseNo { get; set; }
+
+        [JsonProperty(PropertyName = "颜色")]
+        public string ProductNo { get; set; }
+
+        [JsonProperty(PropertyName = "市场价")]
+        public decimal OriginalPrice { get; set; }
+
+        [JsonProperty(PropertyName = "供货价")]
+        public decimal ActualPrice { get; set; }
+    }
+}
