f0d53d32f7a917b8515c67c32e47a6367c9c1a55
yuliang <4896819@qq.com>
2017-03-15 17:09:39 +0800

    1

---
+++ QingFeng.HomeArea/Controllers/StaffController.cs
@@ -0,0 +1,16 @@
+﻿using QingFeng.Common;
+using QingFeng.WebArea.Fillter;
+using System.Web.Mvc;
+
+namespace QingFeng.WebArea.Controllers
+{
+    [AdminAuthorize(AgentEnums.UserRole.Administrator)]
+    public class StaffController : CustomerController
+    {
+        // GET: Staff
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
